a72ec225b28923f477afc71e23918058b413499b
Udobnaja <Udobnaja@gmail.com>
2016-02-18 14:51:08 +0200

    Распространение волны

--- js/solution.js
+++ js/solution.js
@@ -58,6 +58,41 @@
             return (i>=0 && j>=0 && i<arr.length && j<arr[0].length ) ? true : false;
         }
 
+        /**
+         * Функция проходит по лабиринту и для каждой ячейки помещает все соседние точки равные нулю в очередь и присваивает им вес ячейки(родитель) + 1
+         */
+
+        function MarkNeighbors(q){
+
+            q.enqueue({i :start_y, j:start_x}); // Помешаем в очередь стартовую точку 
+
+            var obj, y, x
+            while (q.size()!=0){
+                obj = q.repository;
+                y = obj[q.oldestiNdex].i;
+                x = obj[q.oldestiNdex].j;
+
+                if(CheckPoints(x+1,y,maze) && (maze[y][x+1] === 0) && !((x+1===start_x) && (y === start_y))){
+                        maze[y][x+1] = maze[y][x] + 1;
+                        q.enqueue({i :y, j:x+1});
+                }
+                if(CheckPoints(x,y+1,maze) && (maze[y+1][x] === 0) && !((x===start_x) && (y+1 === start_y))){
+                        maze[y+1][x] = maze[y][x] + 1;
+                        q.enqueue({i :y+1, j:x});
+                }
+                if(CheckPoints(x-1,y,maze) && (maze[y][x-1] === 0) && !((x-1===start_x) && (y === start_y))){
+                        maze[y][x-1] = maze[y][x] + 1;
+                        q.enqueue({i :y, j:x-1});
+                }
+                if(CheckPoints(x,y-1,maze) && (maze[y-1][x] === 0) && !((x===start_x) && (y-1 === start_y)) ){         
+                        maze[y-1][x] = maze[y][x] + 1;
+                        q.enqueue({i :y-1, j:x});
+                }
+
+                q.dequeue();
+            }
+        }
+
         // todo: построить правильный маршрут к выходу
         return [
             [1, 0],
